00a06097915f879f7b909fe6351805faab37afbb
Gyeolee <gyeoleeeeeee@gmail.com>
2023-10-23 09:54:03 +0900

    [ADD] 프로젝트 생성

---
+++ CoreMotionSample/CoreMotionSample/CoreMotionSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMotionSampleApp.swift
+//  CoreMotionSample
+//
+//  Created by Hangyeol on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreMotionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
